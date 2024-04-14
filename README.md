0085374a82e5500d8de6be50fa09d642b45df452
Aetherinox <aetherinox@proton.me>
2024-04-14 01:14:16 -0700

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# noxnode
+<p align="center"><img src="https://github.com/Aetherinox/noxnode/assets/118329232/106bcb32-6c6f-423c-a8c6-c6aee3d31c65" width="860"></p>
+<h1 align="center"><b>Nox NodeJS Template</b></h1>
+
+<div align="center">
+
+![Version](https://img.shields.io/github/v/tag/Aetherinox/noxnode?logo=GitHub&label=version&color=ba5225) ![Downloads](https://img.shields.io/github/downloads/Aetherinox/noxnode/total) ![Repo Size](https://img.shields.io/github/repo-size/Aetherinox/noxnode?label=size&color=59702a) ![Last Commit)](https://img.shields.io/github/last-commit/Aetherinox/noxnode?color=b43bcc)
+
+</div>
+
+---
+
+<br />
+
+# About
+This is a NodeJS application template which includes the following:
+
+<br />
+
+- @rollup/plugin-commonjs
+- @rollup/plugin-image
+- @rollup/plugin-node-resolve
+- @rollup/plugin-replace
+- @rollup/plugin-terser
+- @rollup/plugin-typescript
+- typescript
+- tslib
+- rollup
+- rollup-plugin-license
+- semver
+- uuid
+
+<br />
---
+++ rollup.config.mjs
@@ -0,0 +1,144 @@
+/*
+*   @package:     Noxnode
+*   @module:      obsidian.md
+*   @author:      Aetherinox
+*   @url:         https://github.com/Aetherinox/noxnode
+*/
+
+/*
+*    import
+*/
+
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import moment from 'moment';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import license from 'rollup-plugin-license';
+import image from '@rollup/plugin-image';
+import replace from '@rollup/plugin-replace';
+import { v5 as uuidv5 } from 'uuid';
+import { readFileSync, writeFileSync } from 'fs';
+
+/*
+*    declrations > package.json
+*/
+
+const {
+    name,
+    author,
+    version,
+    repository
+} = JSON.parse( readFileSync( './package.json') );
+
+/*
+*    declrations > constants
+*/
+
+const now             = moment( ).milliseconds( 0 ).toISOString( );
+const build_date      = now;
+const bIsProd         = ( process.env.BUILD === 'production' );
+const bIsDev          = ( process.env.BUILD === 'dev' );
+const year            = moment( now ).year( );
+const build_guid      = uuidv5( ` + repository + `, uuidv5.URL )
+const build_uuid      = uuidv5( version, build_guid )
+const path_save_home  = './'
+const path_save_dist  = 'dist/'
+
+/*
+*    write build id to file
+*
+*    export $(cat .env | xargs)
+*/
+
+const ids = `
+GUID=${ build_guid }
+UUID=${ build_uuid }
+`;
+
+/*
+*    write const ids to .env file
+*/
+
+writeFileSync( ".env", ids,
+{
+    flag: "w"
+})
+
+const asdad = process.env.NAME = "reee"
+
+/*
+*    banner
+*/
+
+const header_banner = `
+@name:        ${ name } v${ version }
+@author:      ${ author }
+@url:         ${ repository.url }
+@copyright:   (c) ${ year } ${ author }
+@license:     MIT
+@build:       ${ build_date }
+@guid:        ${ build_guid }
+@uuid:        ${ build_uuid }
+`;
+
+/*
+*    banner output
+*/
+
+console.log( header_banner );
+console.log( `Running in ${ bIsDev ? 'development' : 'production' } mode` );
+
+/*
+*    rollup config
+*/
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        dir: path_save_home,
+        sourcemap: 'inline',
+        sourcemapExcludeSources: bIsProd,
+        format: 'cjs',
+        exports: 'named'
+    },
+    external: [
+        'uuid',
+    ],
+    plugins: [
+        replace( {
+            preventAssignment: true,
+            values: {
+                "process.env.NODE_ENV": bIsProd ? '"production"' : '"dev"',
+                "process.env.ENV": bIsProd ? '"production"' : '"dev"',
+                "process.env.BUILD": bIsProd ? '"production"' : '"dev"',
+                "process.env.APP_VERSION": `"${ version }"`,
+                "process.env.BUILD_GUID": `"${ build_guid }"`,
+                "process.env.BUILD_UUID": `"${ build_uuid }"`,
+                "process.env.BUILD_DATE": JSON.stringify( moment( now ) ),
+                "process.env.AUTHOR": `"${ author }"`,
+            },
+        } ),
+        typescript( ),
+        nodeResolve( { browser: true } ),
+        commonjs( ),
+        image( ),
+        terser( {
+            ecma: 2020,
+            mangle: { toplevel: true },
+            compress: {
+                module: true,
+                toplevel: true,
+                unsafe_arrows: true
+            },
+            format: { comments: false }
+        } ),
+        license( {
+            sourcemap: true,
+            banner: {
+                content:  `${ header_banner }`,
+                commentStyle: 'regular',
+            },
+        } ),
+    ]
+};
